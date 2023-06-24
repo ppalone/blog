06fad28711adb733de8230bbe2a488e3793d1fa4
ppalone <pranjalalone@gmail.com>
2023-06-24 17:46:42 +0530

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "dracula-soft",
+      wrap: true,
+    }
+  },
+  integrations: [vue()]
+});
