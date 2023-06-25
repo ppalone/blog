6cd0f3f8349bf1a7cbe7e871a979811e9ebd5af3
ppalone <pranjalalone@gmail.com>
2023-06-26 00:27:12 +0530

    change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {},
     fontFamily: {
-      "sans": ["IBM Plex Sans", "sans-serif"],
+      "sans": ["PT Sans", "sans-serif"],
       "mono": ["Inconsolata", "monospace"]
     }
   },
